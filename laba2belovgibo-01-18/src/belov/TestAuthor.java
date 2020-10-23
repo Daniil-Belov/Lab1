54591ba1dfcdcbc915603f836f045383aa87a703
Daniil-Belov <72224473+Daniil-Belov@users.noreply.github.com>
2020-10-23 17:26:38 +0300

    Add files via upload

---
+++ laba2belovgibo-01-18/src/belov/TestAuthor.java
@@ -0,0 +1,9 @@
+package belov;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author a1 = new Author("Belov Daniil", "BelovDaniil@mail.ru", 'M');
+        System.out.println( a1);
+    }
+}
+
